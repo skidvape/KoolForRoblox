ad5958645fcb2421a8facd83e32ab3eca1c1cddb
sstvskids <staavstudios@gmail.com>
2025-07-13 13:33:46 +1000

    Fix

--- games/15999929350.lua
+++ games/15999929350.lua
@@ -26,7 +26,7 @@ local function notif(...)
 	return vape:CreateNotification(...)
 end
 
-local firepart = function(plr: Instance, item: Instance, tog: number): Instance, number
+local firepart = function(plr: Instance, item: Instance, tog: number): Instance
     task.spawn(function()
         if tog == 0 or tog > 1 then return end
 
