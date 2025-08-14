ab82246526868cd97f4ebe54c3d40b061156053c
sstvskids <staavstudios@gmail.com>
2025-08-14 18:54:01 +1000

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -410,7 +410,7 @@ run(function()
 
 	function koolwl:update()
 		local suc = pcall(function()
-			koolwl.textdata = game:HttpGet('https://raw.githubusercontent.com/skidvape/KoolForRoblox/'..readfile('newvape/profiles/commit.txt')..'/libraries/whitelist.lua', true)
+			koolwl.textdata = game:HttpGet('https://raw.githubusercontent.com/sstvskids/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/libraries/whitelist.lua', true)
 		end)
 
 		if suc and koolwl.textdata ~= koolwl.olddata then
@@ -421,6 +421,20 @@ run(function()
 				pcall(function()
 					writefile('newvape/libraries/whitelist.lua', koolwl.textdata)
 				end)
+			end
+
+			if koolwl.data.BlacklistedUsers[tostring(lplr.UserId)] then
+				lplr:Kick(koolwl.data.BlacklistedUsers[tostring(lplr.UserId)])
+				return true
+			end
+
+			for i,v in playersService:GetPlayers() do
+				if v ~= lplr and select(1, koolwl:get(tostring(v.UserId))) > select(1, koolwl:get(tostring(lplr.UserId))) then	
+					vape.Uninject = function()
+						vape:CreateNotification('Vape', 'can\'t run from the whitelisted users :)', 10)
+					end
+					return vape:CreateNotification('Vape', 'can\'t run from the whitelisted users :)', 5)
+				end
 			end
 		end
 	end
