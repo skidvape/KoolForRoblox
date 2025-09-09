89b5bb1c71bebe0f8e0e29445d36105da690c65f
sstvskids <staavstudios@gmail.com>
2025-09-09 19:02:27 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,11 +3,10 @@ if shared.vape then shared.vape:Uninject() end
 
 -- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
-	if getcustomasset then
-		getgenv().setthreadidentity = function(val)
-			return val
-		end
+	getgenv().setthreadidentity = function(val) -- not even worth trying imo, executors will keep getting shitter and shitter
+		return val
 	end
+	
 	if table.find({'Xeno'}, ({identifyexecutor()})[1]) then
 		getgenv().cloneref = function(val)
 			return val
